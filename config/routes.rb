792d01109a142a1870242ddf68dcd7b78cdedacc
Trent Childers <trent65@msn.com>
2015-07-09 21:02:35 -0500

    added access restriction

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
- get '/' => 'bands#index'
- get '/bands' => 'bands#index'
- get '/bands/:id' => 'bands#show'
- post '/bands' => 'bands#create'
- patch '/bands/:id' => 'bands#update'
- delete '/bands/:id' => 'bands#destroy'
+
+namespace :api do
+  namespace :v1 do
+     get '/' => 'bands#index'
+     get '/bands' => 'bands#index'
+     get '/bands/:id' => 'bands#show'
+     post '/bands' => 'bands#create'
+     patch '/bands/:id' => 'bands#update'
+     delete '/bands/:id' => 'bands#destroy'
+   end
+ end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630003014) do
+ActiveRecord::Schema.define(version: 20150710005309) do
 
   create_table "bands", force: :cascade do |t|
     t.string   "name",           limit: 255
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20150630003014) do
     t.integer  "number_members", limit: 4
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      limit: 255
+    t.string   "api_key",    limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
